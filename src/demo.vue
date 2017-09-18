baf242094009f9ef50e16765ba31e3413f2ae4e2
Nikola Kostadinov <nikolakk@gmail.com>
2017-09-18 15:38:57 +0300

    add gh pages docs

--- src/demo.vue
+++ src/demo.vue
@@ -1,49 +1,47 @@
 <template>
-  <div class="hello">
-    <h1>Vue Date Range Picker</h1>
-    <p>
-      Based on <a href="http://www.daterangepicker.com" target="_blank">http://www.daterangepicker.com</a>
-    </p>
+    <div class="hello">
+        <h1>Vue Date Range Picker</h1>
+        <p>
+            Based on <a href="http://www.daterangepicker.com" target="_blank">http://www.daterangepicker.com</a>
+        </p>
 
-    <p>
-      The component is rewritten without jQuery dependancy. Requires only bootstrap and the original datepicker CSS (included).
-    </p>
+        <p>
+            The component is rewritten without jQuery dependancy. Requires only bootstrap and the original datepicker CSS (included).
+        </p>
 
-    <date-range-picker></date-range-picker>
-  </div>
+        <date-range-picker></date-range-picker>
+    </div>
 </template>
 
 <script>
-  import DateRangePicker from './components/DateRangePicker'
+    import DateRangePicker from './components/DateRangePicker'
 
-export default {
-      components: { DateRangePicker },
-  name: 'hello',
-  data () {
-    return {
-
+    export default {
+        components: {DateRangePicker},
+        name: 'hello',
+        data() {
+            return {}
+        }
     }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-a {
-  color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
 </style>
