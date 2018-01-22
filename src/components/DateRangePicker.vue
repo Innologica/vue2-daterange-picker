9676a8e711f09b3e1d2760b3bb5905de2c4039e6
Nikola Kostadinov <nikolakk@gmail.com>
2018-01-22 10:22:55 +0200

    Allow override of locale data

--- src/components/DateRangePicker.vue
+++ src/components/DateRangePicker.vue
@@ -1,174 +1,197 @@
 <template>
     <div style="position: relative; display: inline-block;">
-            <div class="reportrange-text" @click="togglePicker">
-                <slot name="input">
-                    <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;
-                    <span>{{startText}} - {{endText}}</span>
-                    <b class="caret"></b>
-                </slot>
-            </div>
+        <div class="reportrange-text" @click="togglePicker">
+            <slot name="input">
+                <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;
+                <span>{{startText}} - {{endText}}</span>
+                <b class="caret"></b>
+            </slot>
+        </div>
         <transition name="slide-fade" mode="out-in">
-            <div class="daterangepicker dropdown-menu ltr" :class="pickerStyles()" v-show="open">
+            <div class="daterangepicker dropdown-menu ltr" :class="pickerStyles()" v-if="open"
+                 v-on-clickaway="clickAway">
+
                 <div class="calendar left">
-                    <div class="daterangepicker_input">
+                    <div class="daterangepicker_input hidden-xs">
                         <input class="input-mini form-control" type="text" name="daterangepicker_start"
                                :value="startText"/>
                         <i class="fa fa-calendar glyphicon glyphicon-calendar"></i>
                     </div>
                     <div class="calendar-table">
-                        <calendar :monthDate="monthDate" :locale="locale"
+                        <calendar :monthDate="monthDate"
+                                  :locale="locale"
                                   :start="start" :end="end"
                                   @nextMonth="nextMonth" @prevMonth="prevMonth"
                                   @dateClick="dateClick" @hoverDate="hoverDate"
                         ></calendar>
                     </div>
                 </div>
-                <div class="calendar right">
+
+                <div class="calendar right hidden-xs">
                     <div class="daterangepicker_input">
-                        <input class="input-mini form-control" type="text" name="daterangepicker_end" :value="endText"/>
+                        <input class="input-mini form-control" type="text" name="daterangepicker_end"
+                               :value="endText"/>
                         <i class="fa fa-calendar glyphicon glyphicon-calendar"></i>
                     </div>
                     <div class="calendar-table">
-                        <calendar :monthDate="nextMonthDate" :locale="locale"
+                        <calendar :monthDate="nextMonthDate"
+                                  :locale="locale"
                                   :start="start" :end="end"
                                   @nextMonth="nextMonth" @prevMonth="prevMonth"
                                   @dateClick="dateClick" @hoverDate="hoverDate"
                         ></calendar>
                     </div>
                 </div>
+
                 <calendar-ranges :canSelect="in_selection" @clickCancel="open=false"
-                                 @clickApply="clickedApply" :ranges="ranges"></calendar-ranges>
+                                 @clickApply="clickedApply" :ranges="ranges" class=" hidden-xs">
+                </calendar-ranges>
             </div>
         </transition>
     </div>
 </template>
 
 <script>
-    import moment from 'moment'
-    import Calendar from "./Calendar.vue"
-    import CalendarRanges from "./CalendarRanges"
-    import {nextMonth, prevMonth} from './util'
+  import moment from 'moment'
+  import Calendar from './Calendar.vue'
+  import CalendarRanges from './CalendarRanges'
+  import { nextMonth, prevMonth } from './util'
+  import { mixin as clickaway } from 'vue-clickaway'
 
-    export default {
-        components: {Calendar, CalendarRanges},
-        props: {
-            startDate: {
-                default() {
-                    return new Date()
-                }
-            },
-            endDate: {
-                default() {
-                    return new Date()
-                }
-            },
-            ranges: {
-                type: Object,
-                default () {
-                    return {
-                        'Today': new Date(),
-                        'Yesterday': new Date(),
-                        'This month': new Date(),
-                        'This year': new Date(),
-                        'Last week': new Date(),
-                        'Last month': new Date(),
-                    }
-                }
-            },
-            opens: {
-                type: String,
-                default: "right"
-            }
+  export default {
+    components: {Calendar, CalendarRanges},
+    mixins: [clickaway],
+    props: {
+      localeData: {
+        type: Object,
+        default () {
+          return {}
         },
-        data() {
-            let data = {
-                locale: {
-                    direction: 'ltr',
-                    format: moment.localeData().longDateFormat('L'),
-                    separator: ' - ',
-                    applyLabel: 'Apply',
-                    cancelLabel: 'Cancel',
-                    weekLabel: 'W',
-                    customRangeLabel: 'Custom Range',
-                    daysOfWeek: moment.weekdaysMin(),
-                    monthNames: moment.monthsShort(),
-                    firstDay: moment.localeData().firstDayOfWeek()
-                }
-            }
-
-            data.monthDate = new Date(this.startDate)
-            data.start = new Date(this.startDate)
-            data.end = new Date(this.endDate)
-            data.in_selection = false
-            data.open = false
-
-            // update day names order to firstDay
-            if (data.locale.firstDay !== 0) {
-                let iterator = data.locale.firstDay;
-                while (iterator > 0) {
-                    data.locale.daysOfWeek.push(data.locale.daysOfWeek.shift());
-                    iterator--;
-                }
-            }
-            return data
-        },
-        methods: {
-            nextMonth() {
-                this.monthDate = nextMonth(this.monthDate)
-            },
-            prevMonth() {
-                this.monthDate = prevMonth(this.monthDate)
-            },
-            dateClick(value) {
-                if (this.in_selection) {
-                    this.in_selection = false
-                    this.end = new Date(value)
-                } else {
-                    this.in_selection = true
-                    this.start = new Date(value)
-                    this.end = new Date(value)
-                }
-            },
-            hoverDate(value) {
-                let dt = new Date(value)
-                if (this.in_selection && dt > this.start)
-                    this.end = dt
-            },
-            togglePicker() {
-                this.open = !this.open
-            },
-            pickerStyles() {
-                return {
-                    'show-calendar': this.open,
-                    opensright: this.opens == 'right',
-                    opensleft: this.opens == 'left'
-                }
-            },
-            clickedApply() {
-                this.open = false
-                this.$emit('update', { startDate: this.start, endDate: this.end})
-            }
-        },
-        computed: {
-            nextMonthDate() {
-                return nextMonth(this.monthDate)
-            },
-            startText() {
-                return this.start.toLocaleDateString()
-            },
-            endText() {
-                return new Date(this.end).toLocaleDateString()
-            }
-        },
-        watch: {
-            startDate(value) {
-                this.start = new Date(value)
-            },
-            endDate(value) {
-                this.end = new Date(value)
-            }
+      },
+      startDate: {
+        default () {
+          return new Date()
         }
+      },
+      endDate: {
+        default () {
+          return new Date()
+        }
+      },
+      ranges: {
+        type: Object,
+        default () {
+          return {
+            'Today': new Date(),
+            'Yesterday': new Date(),
+            'This month': new Date(),
+            'This year': new Date(),
+            'Last week': new Date(),
+            'Last month': new Date(),
+          }
+        }
+      },
+      opens: {
+        type: String,
+        default: 'center'
+      }
+    },
+    data () {
+      let default_locale = {
+        direction: 'ltr',
+        format: moment.localeData().longDateFormat('L'),
+        separator: ' - ',
+        applyLabel: 'Apply',
+        cancelLabel: 'Cancel',
+        weekLabel: 'W',
+        customRangeLabel: 'Custom Range',
+        daysOfWeek: moment.weekdaysMin(),
+        monthNames: moment.monthsShort(),
+        firstDay: moment.localeData().firstDayOfWeek()
+      }
+
+      // let data = { locale: _locale }
+      let data = { locale: {...default_locale, ...this.localeData}}
+
+      data.monthDate = new Date(this.startDate)
+      data.start = new Date(this.startDate)
+      data.end = new Date(this.endDate)
+      data.in_selection = false
+      data.open = false
+
+      // update day names order to firstDay
+      if (data.locale.firstDay !== 0) {
+        let iterator = data.locale.firstDay
+        while (iterator > 0) {
+          data.locale.daysOfWeek.push(data.locale.daysOfWeek.shift())
+          iterator--
+        }
+      }
+      return data
+    },
+    methods: {
+      nextMonth () {
+        this.monthDate = nextMonth(this.monthDate)
+      },
+      prevMonth () {
+        this.monthDate = prevMonth(this.monthDate)
+      },
+      dateClick (value) {
+        if (this.in_selection) {
+          this.in_selection = false
+          this.end = new Date(value)
+        } else {
+          this.in_selection = true
+          this.start = new Date(value)
+          this.end = new Date(value)
+        }
+      },
+      hoverDate (value) {
+        let dt = new Date(value)
+        if (this.in_selection && dt > this.start)
+          this.end = dt
+      },
+      togglePicker () {
+        this.open = !this.open
+      },
+      pickerStyles () {
+        return {
+          'show-calendar': this.open,
+          opensright: this.opens === 'right',
+          opensleft: this.opens === 'left',
+          openscenter: this.opens === 'center'
+        }
+      },
+      clickedApply () {
+        this.open = false
+        this.$emit('update', {startDate: this.start, endDate: this.end})
+      },
+      clickAway () {
+        if (this.open) {
+          this.open = false
+        }
+      }
+    },
+    computed: {
+      nextMonthDate () {
+        return nextMonth(this.monthDate)
+      },
+      startText () {
+        return this.start.toLocaleDateString()
+      },
+      endText () {
+        return new Date(this.end).toLocaleDateString()
+      }
+    },
+    watch: {
+      startDate (value) {
+        this.start = new Date(value)
+      },
+      endDate (value) {
+        this.end = new Date(value)
+      }
     }
+  }
 
 </script>
 
@@ -199,12 +222,20 @@
 
     .show-calendar {
         display: block;
+        width: auto;
     }
 
     div.daterangepicker.opensleft {
         top: 35px;
         right: 10px;
         left: auto;
+    }
+
+    div.daterangepicker.openscenter {
+        top: 35px;
+        right: auto;
+        left: 50%;
+        transform: translate(-50%, 0);
     }
 
     div.daterangepicker.opensright {
--- src/demo.vue
+++ src/demo.vue
@@ -1,32 +1,85 @@
 <template>
-    <div class="hello">
+    <div class="demo container">
         <h1>Vue Date Range Picker</h1>
         <p>
             Based on <a href="http://www.daterangepicker.com" target="_blank">http://www.daterangepicker.com</a>
         </p>
 
         <p>
-            The component is rewritten without jQuery dependancy. Requires only bootstrap and the original datepicker CSS (included).
+            The component is rewritten without jQuery dependancy. Requires only bootstrap and the original datepicker
+            CSS (included).
         </p>
 
-        <date-range-picker></date-range-picker>
+        <div class="well">
+            <div class="form-horizontal">
+                <div class="form-group">
+                    <label class="col-sm-2 control-label" for="startDate">StartDate</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control" id="startDate" v-model="startDate">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label" for="endDate">EndDate</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control" id="endDate" v-model="endDate">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label">Opens: </label>
+                    <div class="">
+                        <label class="radio-inline">
+                            <input type="radio" name="options" id="option1" value="left" v-model="opens">left
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="options" id="option2" value="center" v-model="opens">center
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="options" id="option3" value="right" v-model="opens">right
+                        </label>
+                    </div>
+                    <span class="help-block">(string: 'left'/'right'/'center') Whether the picker appears aligned to the left, to the right, or centered under the HTML element it's attached to</span>
+                </div>
+            </div>
+        </div>
+
+        <div style="height: 400px;">
+            <date-range-picker
+                    :opens="opens"
+                    :startDate="startDate"
+                    :endDate="endDate"
+                    @update="updateValues"
+            ></date-range-picker>
+        </div>
     </div>
 </template>
 
 <script>
-    import DateRangePicker from './components/DateRangePicker'
+  import DateRangePicker from './components/DateRangePicker'
 
-    export default {
-        components: {DateRangePicker},
-        name: 'hello',
-        data() {
-            return {}
-        }
+  export default {
+    components: {DateRangePicker},
+    name: 'DateRangePickerDemo',
+    data () {
+      //                    :locale-data="{ daysOfWeek: [ 'Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб' ] }"
+      return {
+        opens: 'center',
+        startDate: '2017-09-19',
+        endDate: '2017-10-09'
+      }
+    },
+    methods: {
+      updateValues (values) {
+        console.log(values)
+        this.startDate = values.startDate.toISOString().slice(0, 10)
+        this.endDate = values.endDate.toISOString().slice(0, 10)
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
     h1, h2 {
         font-weight: normal;
     }
