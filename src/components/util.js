96eff5cf68ef82721f5b993efd39fd891c3fa8e4
huba <huba.gaspar@gravityrd.com>
2019-02-08 11:31:44 +0100

    bugfix, nextMonth util fn: returned date is initialized with day=1, as it faultily returned a date with currentMonth+2, if date.getDate() was a higher number then the last day of newMonth

--- src/components/util.js
+++ src/components/util.js
@@ -1,15 +1,19 @@
 const nextMonth = (date) => {
-    let passYear = date.getMonth() === 11
-    let newMonth = passYear ? 0 : date.getMonth() + 1
-    let year = passYear ? date.getFullYear() + 1 : date.getFullYear()
-    return new Date(year, newMonth, date.getDate())
+
+    let passYear = date.getMonth() === 11,
+      year = passYear ? date.getFullYear() + 1 : date.getFullYear(),
+      newMonth = passYear ? 0 : date.getMonth() + 1;
+
+    return new Date(year, newMonth, 1);
 }
 
 const prevMonth = (date) => {
-    let passYear = date.getMonth() === 0
-    let newMonth = passYear ? 11 : date.getMonth() - 1
-    let year = passYear ? date.getFullYear() - 1 : date.getFullYear()
-    return new Date(year, newMonth, date.getDate())
+
+    let passYear = date.getMonth() === 0,
+      newMonth = passYear ? 11 : date.getMonth() - 1,
+      year = passYear ? date.getFullYear() - 1 : date.getFullYear();
+
+    return new Date(year, newMonth, 1);
 }
 
 export { nextMonth, prevMonth }
