06e9dbbed3ab23de356b8c99d1a1176dc3b87980
Kyle Agronick <kyle.agronick@cvscaremark.com>
2019-04-18 22:42:50 -0400

    Fixed 12 hour conversion

--- src/components/CalendarTime.vue
+++ src/components/CalendarTime.vue
@@ -92,9 +92,20 @@
       },
     },
     methods: {
+      getHour() {
+        if (this.hour24) {
+          return this.hour;
+        } else {
+         if (this.hour === 12) {
+           return this.ampm === 'AM' ? 0 : 12;
+         } else {
+           return this.hour + (this.ampm === 'PM' ? 12 : 0);
+         }
+        }
+      },
       onChange () {
         this.$emit('update', {
-          hours: this.hour24? this.hour : (this.hour - 1 + (this.ampm === 'AM'? 0:12)),
+          hours: this.getHour(),
           minutes: this.minute,
           seconds: this.second,
         });
