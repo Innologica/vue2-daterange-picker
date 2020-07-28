00564c28ca0e7f0c69aad8e4a76be1b650d4647b
Gwilyn <gwilyn@karmabunny.com.au>
2020-07-28 10:53:14 +0930

    Improve typings in date_util template.

--- src/components/date_util/template.js
+++ src/components/date_util/template.js
@@ -1,57 +1,57 @@
 const DateUtil = {
   /**
    * Compares if two date1 and date2 are the same with specified granularity(date, month, year)
-   * @param date1
-   * @param date2
-   * @param granularity
-   * @return Boolean returns if the dates are the same with this granularity
+   * @param {Date} date1
+   * @param {Date} date2
+   * @param {'millisecond'|'second'|'minute'|'hour'|'day'|'month'|'quarter'|'year'} granularity
+   * @returns {boolean} returns if the dates are the same with this granularity
    */
   isSame: (date1, date2, granularity) => {},
   /**
    * Get number of days in month specified by year and month
-   * @param year
-   * @param month
-   * @return int number of days
+   * @param {number} year
+   * @param {number} month
+   * @return {number} number of days
    */
   daysInMonth: (year, month) => {},
   /**
    * Return ISO week number for date
-   * @param date
-   * returns int the week number
+   * @param {Date} date
+   * @return {number} the week number
    */
   weekNumber: (date) => {},
   /**
    * Formats the date object with the specified mask.
    * @see https://github.com/felixge/node-dateformat
    *
-   * @param date
-   * @param mask
-   * return String the formatted date
+   * @param {Date} date
+   * @param {string} mask
+   * @return {string} the formatted date
    */
   format: (date, mask) => {},
   /**
    * Get next month from date
-   * @param date
-   * @return Date the first day of the next month
+   * @param {Date} date
+   * @return {Date} the first day of the next month
    */
   nextMonth: (date) => {},
   /**
    * Get previous month from date
-   * @param date
-   * @return Date the first day of the previous month
+   * @param {Date} date
+   * @return {Date} the first day of the previous month
    */
   prevMonth: (date) => {},
   /**
    * Validates a date base on date range [min, max]. Keeps the date in the constraint.
-   * @param newDate
-   * @param min
-   * @param max
-   * @return Date the date after it has been checked if it is in the range specified.
+   * @param {Date} newDate
+   * @param {Date|number} min
+   * @param {Date|number} max
+   * @return {Date} the date after it has been checked if it is in the range specified.
    */
   validateDateRange: (newDate, min, max) => {},
   /**
    * Returns the locale data based on passed options object.
-   * @param options
+   * @param {any} options
    * @return {{cancelLabel: string, applyLabel: string, format: string, daysOfWeek: *, separator: string, customRangeLabel: string, weekLabel: string, direction: string}}
    */
   localeData: (options) => {
@@ -72,8 +72,8 @@ const DateUtil = {
   },
   /**
    * Returns date in the format YYYYMM
-   * @param date
-   * return String the date formatted
+   * @param {Date} date
+   * @return {string} the date formatted
    */
   yearMonth: (date) => {}
 }
